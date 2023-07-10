70b658d1bfc72c12c0b6519f69fba440ef5f8132
Mayantra <87351638+Mayantra@users.noreply.github.com>
2023-07-10 11:18:49 -0600

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('cambio').addEventListener('click', function(){
+    this.innerHTML= /*html*/`
+    <a href="https://youtu.be/_Yhyp-_hX2s">Entra aqui porfavor</a>
+    <p>Hola Oscal</p>
+    `
+})
